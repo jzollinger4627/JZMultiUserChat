288311712a1451b1ceb8a974ca906f4e4f3fbe7c
jzollinger4627 <jzoll4627@westada.org>
2019-04-29 13:03:33 -0600

    Taking the Multi Threading into a new class and got rid of stack of statics

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -16,32 +16,13 @@ public class ServerMain {
                 System.out.println("About to accept client connection");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accept connection from " + clientSocket);
-                new Thread() {
-                  public void run() {
-                      try {
-                          handleClientSocket(clientSocket);
-                      } catch (IOException e) {
-                          e.printStackTrace();
-                      } catch (InterruptedException e) {
-                          e.printStackTrace();
-                      }
-                  }
-                }.start();
+                ServerWorker Worker = new ServerWorker(clientSocket);
+                Worker.start();
             }
         }catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
-
-    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-        OutputStream outputStream = clientSocket.getOutputStream();
-        for (int i=0;i<10;i++) {
-            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
-            Thread.sleep(1000);
-        }
-        outputStream.close();
-        clientSocket.close();
     }
 
 }
---
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -0,0 +1,36 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Date;
+
+public class ServerWorker extends Thread {
+
+    private final Socket clientSocket;
+
+    public ServerWorker(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public void run() {
+        try {
+            handleClientSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClientSocket() throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for (int i=0;i<10;i++) {
+            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        outputStream.close();
+        clientSocket.close();
+    }
+
+}
